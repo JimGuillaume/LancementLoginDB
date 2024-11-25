5c15049b64439fb7d36b585820d80288c5cd439d
Jim <148584426+JimGuillaume@users.noreply.github.com>
2024-11-25 20:35:01 +0100

    ADD : SignIn Fct ADD : Security ADD : Better SyntaxCheck

--- LancementLoginDB/UserSignUp.cs
+++ LancementLoginDB/UserSignUp.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LancementLoginDB
+﻿namespace LancementLoginDB
 {
   public class UserSignUp
   {
@@ -21,10 +15,7 @@ namespace LancementLoginDB
     }
 
     //CONSTRUCTOR  & DESTRUCTOR
-    public UserSignUp()
-    {
-      DefaultUser();
-    }
+    public UserSignUp() => DefaultUser();
 
     ~UserSignUp()
     {
@@ -40,35 +31,23 @@ namespace LancementLoginDB
       //Pas de caractères spéciaux
       //A-Z ; a-z ; 0-9
 
-      if (t_UID == "")
-      {
-        throw new UserSignUpException(1, "Le Nom d'utilisateur ne peut pas être vide");
-      }
-      else if (t_UID.Length < 3 || t_UID.Length > 25)
-      {
-        throw new UserSignUpException(2, "Le Nom d'utilisateur doit être d'une longeur 3 - 25");
-      }
-      else if (t_UID.Any(ch => !char.IsLetterOrDigit(ch)))
-      {
-        throw new UserSignUpException(3, "Le Nom d'utilisateur ne doit pas contenir de caractères spéciaux");
-      } else this.UserId = t_UID;
+      SyntaxCheck UIDCheck = new();
+
+      this.UserId = UIDCheck.UIDCheck(t_UID, 1);
     }
 
     public void Set_UPSWD(string t_UPSWD)
     {
-      if (t_UPSWD == "null")
-      {
-        throw new UserSignUpException(4, "Le Mot de passe ne peut pas être vide");
-      }
-      else if (t_UPSWD.Length < 5 || t_UPSWD.Length > 25)
-      {
-        throw new UserSignUpException(5, "Le mot de passe doit être entre 5 & 25 caractères");
-      } else this.UserPswd = t_UPSWD;
+      SyntaxCheck PasswordCheck = new();
+
+      this.UserPswd = PasswordCheck.PasswordCheck(t_UPSWD, 1);
     }
 
     public void Set_UMail(string t_UMail)
     {
-      this.UserEmail = t_UMail;
+      SyntaxCheck eMailCheck = new();
+
+      this.UserEmail = eMailCheck.EmailCheck(t_UMail, 1);
     }
 
 
@@ -88,6 +67,10 @@ namespace LancementLoginDB
     {
       return this.UserEmail;
     }
+
+
+
+    //MISC
 
 
   }
